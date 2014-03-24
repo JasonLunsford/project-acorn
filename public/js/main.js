95861102afed775e37da742c7dbb190527027e6a
Jason Lunsford <miyamota@yahoo.com>
2014-03-24 11:44:02 -0400

    improved the flag check logic, added comment block

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,39 @@
+/*
+   Project Acorn
 
-// Will need to replace with Angular directive
+   Author: Jason Lunsford
+   Version: 1.0
+   
+   main.js
+
+   note: will need to convert most / all of this code to an Angular directive
+
+*/
 
 "use strict";
 
 (function($) {
 
-	// Non-Control Elements (jQuery element references)
+	// Element references
 
 
    // Common Variables
    var windowState;
 
-   /* CSS Settings (etc)
-   ---------------------------------------- */
+   // CSS Settings (for example)
 
-
-
-   /* End Settings -------------------------*/
-
-   // Check flag state on load, convert containers from fluid to fixed on truthy return
-   if ( checkForDesktop() ) {
-      //$mainContent.removeClass("container-fluid").addClass("container");
-   }
-
-   /* As window resizes check for truthy return from checkForDesktop. if/when it happens, convert
-      container from fluid to fixed */
+   
    $(window).resize(function() {
-      if ( checkForDesktop() ) {
-         //$mainContent.removeClass("container-fluid").addClass("container");
-      } else {
-         //$mainContent.removeClass("container").addClass("container-fluid");
-      }
+      // check breakpoint flag & take action
    });
 
+   function checkDevice() {
+      var windowState = window.getComputedStyle(document.body,':before').getPropertyValue('content');
+      var devices = ["mobile", "tablet", "desktop", "widescreen"];
 
-   // Simple function to keep track of the content flag state within the body:after pseudo element
-   // bug in Chrome, may not work as expected - TEST (discovered 03.21.2014)
-   function checkForDesktop() {
-      var windowState = window.getComputedStyle(document.body,':after').getPropertyValue('content');
-      return ( windowState.indexOf("desktop") != -1 ) ? true : false
+      for (var i = 0; i < devices.length; i++) {
+         if ( windowState.indexOf(devices[i]) != -1 ) { return devices[i]; }
+      }
    }
 
 })(jQuery);
